d120e00bd4ca314607b4076c9256e8fc8a409f74
Saurabh Mishra <saurabhmishramisau8322@gmail.com>
2025-03-23 20:18:37 +0530

    Created a CPP file

---
+++ Kth Largest Sum in a Binary Tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        long long kthLargestLevelSum(TreeNode* root, int k) {
+            vector<long long> ans;
+            queue<TreeNode*> q;
+            q.push(root);
+            while(!q.empty()){
+                int n=q.size();
+                long long temp=0;
+                for(int i=0;i<n;i++){
+                    temp+=q.front()->val;
+                    if(q.front()->left) q.push(q.front()->left);
+                    if(q.front()->right) q.push(q.front()->right);
+                    q.pop();
+                }
+                ans.push_back(temp);
+            }
+            if (k > ans.size()) return -1;
+            sort(ans.begin(),ans.end(),greater<long long>());
+            return ans[k-1];
+    
+    
+        }
+    };
