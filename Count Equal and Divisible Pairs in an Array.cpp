a06bd8e11552cd7a09a3e6e9b905076f41444765
Saurabh Mishra <saurabhmishramisau8322@gmail.com>
2025-04-17 23:31:14 +0530

    Created a CPP file

---
+++ Count Equal and Divisible Pairs in an Array.cpp
@@ -0,0 +1,16 @@
+//Sexy Brute force
+class Solution {
+    public:
+        int countPairs(vector<int>& nums, int k) {
+            int count=0;
+            int n=nums.size();
+            for(int i=0;i<n;i++){
+                for(int j=i+1;j<n;j++){
+                    if(nums[i]==nums[j] && ((i*j)%k)==0){
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    };
